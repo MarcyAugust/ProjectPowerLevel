b15b9b94944afb2a1567834f44721f556d13a52d
MarcyAugust <51036202+MarcyAugust@users.noreply.github.com>
2021-05-26 16:37:20 -0700

    Delete ConfigSetup.py

--- ConfigSetup.py
+++
@@ -1,8 +0,0 @@
-import configparser
-
-config = configparser.ConfigParser()
-config['DEFAULT'] = {'App_Key': 'my_app_key',
-					 'App_Secret': 'my_app_secret',
-					  'User_IRC_Token': 'user_irc_token'}
-with open('config.ini', 'w') as configfile:
-	config.write(configfile)
