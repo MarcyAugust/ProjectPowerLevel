8a79dd60a5ade22bc1f63d6091607ca2272b04dc
MarcyAugust <marcyauguststream@gmail.com>
2021-05-26 17:25:25 -0700

    Added Config pulling

--- AuthenticateStreamer.py
+++ AuthenticateStreamer.py
@@ -2,6 +2,7 @@ from twitchAPI.twitch import Twitch
 from twitchAPI.oauth import UserAuthenticator
 from twitchAPI.oauth import refresh_access_token
 from twitchAPI.types import AuthScope
+import configparser
 from pprint import pprint
 
 
@@ -10,15 +11,19 @@ from pprint import pprint
 #Or just first time program starts, trigger get tokens code -> save to file -> comment that out and assume you'll always have them and always load from file
 #From Kelgand.
 
+parser = configparser.ConfigParser()
+parser.read('config.ini')
 
 #parameters: app_key, app_secret
-twitch = Twitch('my_app_key', 'my_app_secret')
+twitch = Twitch(parser.get('DEFAULT', 'app_key'), parser.get('DEFAULT', 'app_secret'))
 
+print(twitch)
 
 target_scope = [AuthScope.USER_READ_SUBSCRIPTIONS]
 auth = UserAuthenticator(twitch, target_scope, force_verify=False)
 # this will open your default browser and prompt you with the twitch verification website
 token, refresh_token = auth.authenticate()
+
 #new_token, new_refresh_token = refresh_access_token(refresh_token, 'client_id', 'client_secret')
 #pprint(new_token, new_refresh_token_)
 
